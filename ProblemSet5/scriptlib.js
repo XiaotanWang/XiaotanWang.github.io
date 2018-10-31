39286c8467de5e42f954d001c15695c630380561
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-10-31 13:26:40 +0800

    Update

--- ProblemSet5/scriptlib.js
+++ ProblemSet5/scriptlib.js
@@ -7,37 +7,6 @@ $(document).ready(function(){
         //https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Photo%20Collection?api_key=key0BvxEDq9DYkV7a"
         //"https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
         var dataSet = [];
-        $("button#get_data2").click(function(){
-          dataSet=[];
-          airtable_read_endpoint = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
-          $.getJSON(airtable_read_endpoint, function(result) {
-                 $.each(result.records, function(key,value) {
-                     items = [];
-                         items.push(value.fields.Rank)//Name);
-                         //items.push(value.fields.Restaurant_Address)//Completed);
-                         //items.push(value.fields.Restaurant_Special_name)//Time_Estimate);
-                         //items.push(value.fields.Average_Price)//converted);
-                         dataSet.push(items);
-                  }); // end .each
-       var chart = c3.generate({
-
-         data: {
-           columns:
-               ['Rank', dataSet]
-               //['sample2', 130, 300, 200, 500, 250, 350]
-
-       },
-       axis: {
-           x: {
-               label: 'X Label'
-           },
-           y: {
-               label: 'Y Label'
-           }
-       }
-   });
-          }); // end .getJSON
-        });//end get_data2
 
         $("button#get_data1").click(function(){
           dataSet=[];
