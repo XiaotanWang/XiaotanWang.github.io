1a5f96b4a7cd1e835451c32699036e3489bf0e2d
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-10-31 16:17:23 +0800

    Update

--- ProblemSet5/scriptlib.js
+++ ProblemSet5/scriptlib.js
@@ -9,35 +9,7 @@ $(document).ready(function(){
         var dataSet = [];
         var dataChart = [];
         var dataCate = [];
-        $("button#get_data2").click(function(){
-          airtable_read_endpoint ="https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
-          $.getJSON(airtable_read_endpoint, function(result) {
-                 $.each(result.records, function(key,value) {
-                     dataChart = [];
-                     dataCate = [];
-                         dataChart.push(value.fields.Travel_times)//Name);
-                         //items.push(value.fields.Notes)//Completed);
-                         //items.push(value.fields.Rank)//Time_Estimate);
-                         //items.push(value.fields.Travel_times)//converted);
-                         //dataSet.push(items);
-                         dataCate.push(value.fields.Name)
-                  }); // end .each
-        });
-        var chart = c3.generate({
-          data: {
-                  columns: dataChart
 
-        },
-        axis: {
-          x: {
-               type: 'category',
-               categories: dataCate
-        },
-          y: {
-               label: 'Y Label'
-        }
-    }
-});
         $("button#get_data1").click(function(){
           //dataSet=[];
           airtable_read_endpoint ="https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
@@ -67,4 +39,4 @@ $(document).ready(function(){
           });
       });//end get_data1
 }); // button ready
-});//document ready
+//});//document ready
