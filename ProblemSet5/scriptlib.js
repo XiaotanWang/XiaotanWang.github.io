548f39574e9d1e226019b9008f7328885054a4cc
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-10-31 14:54:00 +0800

    Update problem set5

--- ProblemSet5/scriptlib.js
+++ ProblemSet5/scriptlib.js
@@ -7,9 +7,39 @@ $(document).ready(function(){
         //https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Photo%20Collection?api_key=key0BvxEDq9DYkV7a"
         //"https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
         var dataSet = [];
+        var dataChart = [];
+        var dataCate = [];
+        $("button#get_data2").click(function(){
+          airtable_read_endpoint ="https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
+          $.getJSON(airtable_read_endpoint, function(result) {
+                 $.each(result.records, function(key,value) {
+                     dataChart = [];
+                     dataCate = [];
+                         dataChart.push(value.fields.Travel_times)//Name);
+                         //items.push(value.fields.Notes)//Completed);
+                         //items.push(value.fields.Rank)//Time_Estimate);
+                         //items.push(value.fields.Travel_times)//converted);
+                         //dataSet.push(items);
+                         dataCate.push(value.fields.Name)
+                  }); // end .each
+        });
+        var chart = c3.generate({
+          data: {
+                  columns: dataChart
 
+        },
+        axis: {
+          x: {
+               type: 'category',
+               categories: dataCate
+        },
+          y: {
+               label: 'Y Label'
+        }
+    }
+});
         $("button#get_data1").click(function(){
-          dataSet=[];
+          //dataSet=[];
           airtable_read_endpoint ="https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
           $.getJSON(airtable_read_endpoint, function(result) {
                  $.each(result.records, function(key,value) {
