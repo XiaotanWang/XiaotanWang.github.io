30f55ec8125035476f1d85b246fdd4c77c32fbd0
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-11-08 19:19:34 +0800

    Update.

--- ProblemSet6/scriptlib.js
+++ ProblemSet6/scriptlib.js
@@ -2,13 +2,24 @@ $(document).ready(function(){
 //$("button#get_data2").click(function() {
         var items = [];
         var mapitems = [];
-        var i = 0;
         var airtable_read_endpoint = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
         //"https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Food_Recommendation?api_key=key0BvxEDq9DYkV7a"//&maxRecords=3&view=Grid%20view"
         //https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Photo%20Collection?api_key=key0BvxEDq9DYkV7a"
         //"https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
         var dataSet = [];
         var data = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+               $.each(result.records, function(key,value) {
+               mapitems = {};
+                   mapitems["name"] = value.fields.Name;
+                   mapitems["image_url"] = value.fields.url;
+                   mapitems["url"] = value.fields.recurl;
+                   mapitems["latitud"] = value.fields.Lat;
+                   mapitems["longitud"] = value.fields.Lng;
+                   data.push(mapitems);
+                   console.log(mapitems);
+                }); // end .each
+    });
 
         $("button#get_data1").click(function(){
           //dataSet=[];
@@ -37,29 +48,22 @@ $(document).ready(function(){
                });
           });
       });// button ready
-      $("button#get_map1").click(function(){
-        $.getJSON(airtable_read_endpoint, function(result) {
-               $.each(result.records, function(key,value) {
-               mapitems = {};
-                   mapitems["name"] = value.fields.Name;
-                   mapitems["url"] = value.fields.url;
-                   //items["image_url"] = value.fields.image_url;
-                   mapitems["latitud"] = value.fields.Lat;
-                   mapitems["longitud"] = value.fields.Lng;
-                   data.push(mapitems);
-                   console.log(mapitems);
+      //$("button#get_map1").click(function(){
+//end getjson
+//});
 
-                   }
-                }); // end .each
-}); //end getjson
-var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
-var map = L.map('map')
-   .addLayer(mapboxTiles)
-   .setView([22.287111, 114.191667], 13);
+$("button#get_map1").click(function(){
+    var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
+    var map = L.map('map')
+       .addLayer(mapboxTiles)
+       .setView([31.228611, 121.474722], 5);
   for (var i in data) {
   var latlng = L.latLng({ lat: data[i].latitud, lng: data[i].longitud });
   L.marker( latlng )
-    .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + data[i].url+'" width = "80px"><br>'+data[i].name + '</a>' )
-    .addTo(map);
-});//button ready
+          .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + '<img src="' + data[i].image_url+'" width = "80px"><br>'+data[i].name + '</a>' )
+          .addTo(map);
+  //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+}
+});
+//button ready
 });//document ready
