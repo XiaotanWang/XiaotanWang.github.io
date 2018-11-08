299d4ae9e3f7a679b659ca52a14fbb3b05996c9a
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-11-08 18:04:16 +0800

    Update

--- ProblemSet6/scriptlib.js
+++ ProblemSet6/scriptlib.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         var items = [];
         var mapitems = [];
         var i = 0;
-        var airtable_read_endpoint = "";
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
         //"https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Food_Recommendation?api_key=key0BvxEDq9DYkV7a"//&maxRecords=3&view=Grid%20view"
         //https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Photo%20Collection?api_key=key0BvxEDq9DYkV7a"
         //"https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
@@ -12,7 +12,6 @@ $(document).ready(function(){
 
         $("button#get_data1").click(function(){
           //dataSet=[];
-          airtable_read_endpoint ="https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
           $.getJSON(airtable_read_endpoint, function(result) {
                  $.each(result.records, function(key,value) {
                      items = [];
