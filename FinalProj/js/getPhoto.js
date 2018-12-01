39e45b5b929a4937ba8c5173b2b5e6f9d40a964f
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-12-01 16:48:39 +0800

    Very much

--- FinalProj/js/getPhoto.js
+++ FinalProj/js/getPhoto.js
@@ -1,85 +1,80 @@
 $(document).ready(function(){
 //$("button#get_data2").click(function() {
-        var items = [];
-        var mapitems = [];
-        var airtable_read_endpoint = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Place?api_key=key0BvxEDq9DYkV7a";
+        var nameitems1 = [];
+        var photoitems1 = [];
+        var nameitems2 = [];
+        var photoitems2 = [];
+        var airtable_read_endpoint1 = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Category?api_key=key0BvxEDq9DYkV7a";
+        var airtable_read_endpoint2 = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Style?api_key=key0BvxEDq9DYkV7a";
         //"https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Food_Recommendation?api_key=key0BvxEDq9DYkV7a"//&maxRecords=3&view=Grid%20view"
         //https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Photo%20Collection?api_key=key0BvxEDq9DYkV7a"
         //"https://api.airtable.com/v0/appM38HXlEVhxmnqx/Tasks?api_key=keyTcsTzckqyBTlk8&sortField=_createdTime&sortDirection=desc";
-        var dataSet = [];
         var data = [];
-        $.getJSON(airtable_read_endpoint, function(result) {
+        var dataN = [];
+        var data2 = [];
+        var dataN2 = [];
+        $.getJSON(airtable_read_endpoint1, function(result) {
                $.each(result.records, function(key,value) {
-               mapitems = [];
+               photoitems1 = [];
+               nameitems1 = [];
                    //mapitems["name"] = value.fields.Name;
-                   mapitems.push(value.fields.url);
-                   //mapitems["url"] = value.fields.recurl;
-                   //mapitems["latitud"] = value.fields.Lat;
-                   //mapitems["longitud"] = value.fields.Lng;
-                   data.push(mapitems);
-                   //console.log(mapitems);
+                   photoitems1.push(value.fields.url);
+                   nameitems1.push(value.fields.Name);
+                   data.push(photoitems1);
+                   dataN.push(nameitems1);
                 }); // end .each
     });
-    //for (var i = 0; i < mapitem.length; i++) {
-    $("a#test1").click(function(){
-      $("#test").attr ({
-    "src" : mapitems[0]
-    });
+
+    $.getJSON(airtable_read_endpoint2, function(result) {
+          $.each(result.records, function(key,value) {
+            photoitems2 = [];
+            nameitems2 = [];
+           //mapitems["name"] = value.fields.Name;
+              photoitems2.push(value.fields.url);
+              nameitems2.push(value.fields.Name);
+              data2.push(photoitems2);
+              dataN2.push(nameitems2);
+            }); // end .each
   });
-  
-        $("button#get_data1").click(function(){
-          //dataSet=[];
-          $.getJSON(airtable_read_endpoint, function(result) {
-                 $.each(result.records, function(key,value) {
-                     items = [];
-                         items.push(value.fields.Name)//Name);
-                         //items.push(value.fields.Notes)//Completed);
-                         //items.push(value.fields.Rank)//Time_Estimate);
-                         //items.push(value.fields.Travel_times)//converted);
-                         //dataSet.push(items);
-                  }); // end .each
-                });
 
-               $("#example").DataTable({
-                   data: dataSet,
-                   retrieve: true,
-                   columns: [
-                       { title: "Place",
-                         defaultContent:""},
-                       { title: "Notes",
-                         defaultContent:"" },
-                       { title: "Recommendation Rank",
-                         defaultContent:""},
-                       { title: "Mentioned Times",
-                         defaultContent:""},
-                   ]
-               });
-          });
-      });// button ready
-      //$("button#get_map1").click(function(){
-//end getjson
-//});
+    //for (var i = 0; i < mapitem.length; i++) {
+    $("a#show").click(function(){
 
-$("a#get_map1").click(function(){
-    var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
-    var map = L.map('map')
-       .addLayer(mapboxTiles)
-       .setView([31.228611, 121.474722], 5);
+    //$("a#test1").click(function(){
+      $("#get0").attr ({"src" : data[0]});$("#name0").text(dataN[0]);
+      $("#get1").attr ({"src" : data[1]});$("#name1").text(dataN[1]);
+      $("#get2").attr ({"src" : data[2]});$("#name2").text(dataN[2]);
+      $("#get3").attr ({"src" : data[3]});$("#name3").text(dataN[3]);
+      $("#get4").attr ({"src" : data[4]});$("#name4").text(dataN[4]);
+      $("#get5").attr ({"src" : data[5]});$("#name5").text(dataN[5]);
+      $("#get6").attr ({"src" : data[6]});$("#name6").text(dataN[6]);
+      $("#get7").attr ({"src" : data[7]});$("#name7").text(dataN[7]);
+      $("#get8").attr ({"src" : data[8]});$("#name8").text(dataN[8]);
+      $("#get9").attr ({"src" : data[9]});$("#name9").text(dataN[9]);
+      $("#get10").attr ({"src" : data[10]});$("#name10").text(dataN[10]);
+      $("#get11").attr ({"src" : data[11]});$("#name11").text(dataN[11]);
+      $("#get12").attr ({"src" : data[12]});$("#name12").text(dataN[12]);
+      $("#get13").attr ({"src" : data[13]});$("#name13").text(dataN[13]);
+      $("#get14").attr ({"src" : data[14]});$("#name14").text(dataN[14]);
+      $("#get15").attr ({"src" : data[15]});$("#name15").text(dataN[15]);
 
-       // create custom icon
-    var Icon = L.icon({
-    iconUrl: '../FinalProj/img/icon2.png',
-    //iconSize: [55, 55], // size of the icon
-    popupAnchor: [0,-15]
-    });
-
-  for (var i in data) {
-  var latlng = L.latLng({ lat: data[i].latitud, lng: data[i].longitud });
-  L.marker( latlng,{icon: Icon} )
-          .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + '<img src="' + data[i].image_url+'" width = "80px"><br>'+data[i].name + '</a>' )
-          .addTo(map);
-  //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-}
+      $("#image").show();
+  //$("a#test2").click(function(){
+    $("#2get0").attr ({"src" : data2[0]});$("#2name0").text(dataN2[0]);
+    $("#2get1").attr ({"src" : data2[1]});$("#2name1").text(dataN2[1]);
+    $("#2get2").attr ({"src" : data2[2]});$("#2name2").text(dataN2[2]);
+    $("#2get3").attr ({"src" : data2[3]});$("#2name3").text(dataN2[3]);
+    $("#2get4").attr ({"src" : data2[4]});$("#2name4").text(dataN2[4]);
+    $("#2get5").attr ({"src" : data2[5]});$("#2name5").text(dataN2[5]);
+    $("#2get6").attr ({"src" : data2[6]});$("#2name6").text(dataN2[6]);
+    $("#2get7").attr ({"src" : data2[7]});$("#2name7").text(dataN2[7]);
+    $("#2get8").attr ({"src" : data2[8]});$("#2name8").text(dataN2[8]);
+    $("#2get9").attr ({"src" : data2[9]});$("#2name9").text(dataN2[9]);
+    $("#2get10").attr ({"src" : data2[10]});$("#2name10").text(dataN2[10]);
+    $("#2get11").attr ({"src" : data2[11]});$("#2name11").text(dataN2[11]);
+    $("#2get12").attr ({"src" : data2[12]});$("#2name12").text(dataN2[12]);
+    $("#2get13").attr ({"src" : data2[13]});$("#2name13").text(dataN2[13]);
+    $("#2get14").attr ({"src" : data2[14]});$("#2name14").text(dataN2[14]);
+    $("#2get15").attr ({"src" : data2[15]});$("#2name15").text(dataN2[15]);
 });
-//button ready
-//document ready
+)};
