56d713fa8dc329a792dc09e95590fabb5cd460f3
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-12-02 19:05:55 +0800

    Update getDevice.js

--- FinalProj/js/getDevice.js
+++ FinalProj/js/getDevice.js
@@ -39,47 +39,7 @@ $("a#getDevice").click(function() {
                          title: "Tasks for Each Stage:",
                      }
                  });
-
-               $("#example").DataTable({
-                   data: dataSet,
-                   retrieve: true,
-                   columns: [
-                       { title: "Place",
-                         defaultContent:""},
-                       { title: "Notes",
-                         defaultContent:"" },
-                       { title: "Recommendation Rank",
-                         defaultContent:""},
-                       { title: "Mentioned Times",
-                         defaultContent:""},
-                   ]
                });
-          });
-      });// button ready
-      //$("button#get_map1").click(function(){
-//end getjson
-//});
-
-$("a#get_map1").click(function(){
-    var mapboxTiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a>', maxZoom: 18,})
-    var map = L.map('map')
-       .addLayer(mapboxTiles)
-       .setView([31.228611, 121.474722], 5);
-
-       // create custom icon
-    var Icon = L.icon({
-    iconUrl: '../FinalProj/img/icon2.png',
-    //iconSize: [55, 55], // size of the icon
-    popupAnchor: [0,-15]
-    });
-
-  for (var i in data) {
-  var latlng = L.latLng({ lat: data[i].latitud, lng: data[i].longitud });
-  L.marker( latlng,{icon: Icon} )
-          .bindPopup( '<a href="' + data[i].url + '" target="_blank">' + '<img src="' + data[i].image_url+'" width = "80px"><br>'+data[i].name + '</a>' )
-          .addTo(map);
-  //marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-}
-});
-//button ready
-});//document ready
+             });
+           });
+           
