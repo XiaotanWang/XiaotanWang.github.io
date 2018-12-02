8d5e12ec38e8dc6044bd9220a050ff7d0aca65d7
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-12-02 11:31:43 +0800

    Update getDevice.js

--- FinalProj/js/getDevice.js
+++ FinalProj/js/getDevice.js
@@ -23,9 +23,9 @@ $("a#getDevice").click(function() {
                          dataSetN.push(itemsN);
                          dataSetP.push(itemsP);
                          dataSetU.push(itemsU);
-                         console.log(dataSetP);
+                         console.log(itemsP);
                   }); // end .each
-                  //console.log(dataSet);
+                  console.log(dataSetP);
                   var chart = c3.generate({
                     data: {
                         columns: dataSetP,
