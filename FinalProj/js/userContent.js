da6f803f55e999dd5436198199ca0d77020ade53
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-12-02 21:10:43 +0800

    update

--- FinalProj/js/userContent.js
+++ FinalProj/js/userContent.js
@@ -16,25 +16,32 @@ $(document).ready(function(){
                   $.getJSON(airtable_read_endpoint, function(result) {
                          $.each(result.records, function(key,value) {
                              items = [];
-                                 items.push(value.fields.url)//Name);
-                                 //items.push(value.fields.Notes)//Completed);
+                             picitems = [];
+                                 picitems.push(value.fields.url)//Name);
+                                 items.push(value.fields.Name)//Completed);
                                  //items.push(value.fields.Rank)//Time_Estimate);
                                  //items.push(value.fields.Travel_times)//converted);
+                                 data.push(picitems);
                                  dataSet.push(items);
+                                 console.log(data);
+                                 console.log(dataSet);
                           });
+                          console.log(picitems);
+                          console.log(items);
                         });// end .each
 
                       });
 
                   $("button#get_data2").click(function(){
                   for (var i = 0; i < dataSet.length; i++) {
-                   if (i==2) {
-                    //  user=dataSet[i];
-                      $("#2get0").attr ({"src" : dataSet[i]});
-                    break;
+                   if ($("#form7").val() == dataSet[i]) {
+                      break;
                     }
-                  }
-                });
+                    $("#2get0").attr ({"src" : data[i]});
+                    //user = dataSet[i];
+                 }
+                 });
+
 
                 $("button#get_data3").click(function(){
                   $.getJSON(airtable_read_endpoint, function(result) {
@@ -50,10 +57,11 @@ $(document).ready(function(){
                       });
                         $("button#get_data4").click(function(){
                           for (var j = 0; j < data.length; j++) {
-                            if (data[j]==user) {
-                              $("#2get0").attr ({"src" : data[1]});
+                            if (data[j] == user) {
+
                               break;
                             }
+                            $("#2get0").attr ({"src" : data[j]});
                           }
                         });
 
