f611119222e38161cf43960e8112078868ba01ce
XiaotanWang <XiaotanWang@users.noreply.github.com>
2018-12-03 16:17:11 +0800

    Update device chart

--- FinalProj/js/getDevice.js
+++ FinalProj/js/getDevice.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 $("a#getDevice").click(function() {
+  $("#myChart").toggle();
         var items = [];
         var mapitems = [];
         var airtable_read_endpoint = "https://api.airtable.com/v0/appGtLX4GzOC4bWpd/Device?api_key=key0BvxEDq9DYkV7a";
@@ -16,7 +17,7 @@ $("a#getDevice").click(function() {
                      itemsP = [];
                      itemsU = [];
                          itemsN.push(value.fields.Name)//Name);
-                         itemsN.push(value.fields.Price)
+                         itemsP.push(value.fields.Price)
                          //itemsU.push(value.fields.Name)
                          itemsU.push(value.fields.UsedTimes)//Completed);
                          //items.push(value.fields.Rank)//Time_Estimate);
@@ -31,30 +32,107 @@ $("a#getDevice").click(function() {
                   console.log(dataSetP);
                   console.log(dataSetU);
                   console.log(dataSetN)
-                var chart = c3.generate({
-                     data: {
-                        //x:'x',
-                         columns: dataSetN,
-
-                          // ['x', dataSetN],
-                           //['Price',dataSetP],
-                          // ['Used Times',dataSetU],
 
 
-                         //names: {
-                           //dataSetP:'Price',
-                           //dataSetU:'Used Times'
-                         //},
-                         type : 'bar'
-                     },
-                     axis: {
-                       x: {label: 'Devices'},
-                       y: {label: 'Price/$'}
-                     },
-                     //bar: {
-                        // title: "Tasks for Each Stage:",
-                  //   }
-                 });
-               });
-             });
-           });
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: dataSetN,
+        datasets: [{
+            label: 'Price',
+            yAxisID: 'Price',
+            data: dataSetP,
+
+            borderColor: [
+                            'rgba(100, 161, 157, 0.75)',
+                          ],
+            borderWidth: 3,
+            fill: false
+        },
+        {
+          label: 'Used Times',
+          yAxisID: 'UsedTimes',
+          data: dataSetU,
+          borderColor: [
+                          'rgba(255, 99, 20, 0.75)',
+                        ],
+          borderWidth: 3,
+          fill: false
+        }
+      ]
+    },
+    options: {
+//scale
+        scales: {
+          xAxes: [{
+                // 背景网格
+                gridLines: {
+                    // 取消x轴的竖线
+                    display: false,
+                    // 设置x轴线颜色
+                  //  color: "black",
+                },
+                // x轴标题
+                scaleLabel: {
+                    display: true,
+                    labelString: "Devices Name",
+                },
+            }],
+            yAxes: [{
+              id: 'Price',
+              type: 'linear',
+              position: 'left',
+                ticks: {
+                    min:100,
+                    setpSize:1500,
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: "Price/$",
+                },
+            },
+            {
+              id: 'UsedTimes',
+              type: 'linear',
+              position: 'right',
+                ticks: {
+                    beginAtZero:true
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: "Used Times",
+                },
+            }
+          ],
+          //end scale
+          legend: {
+            labels: {
+                padding: 30,
+                // 使用圆形样式
+                usePointStyle: true
+            },
+            // 显示位置
+            position: 'bottom'
+        },
+
+          tooltips: {
+            enabled: true,
+    // 同时显示x轴上的数据
+    // 这里鼠标移到对应点上会同时显示english和math的成绩
+            mode: 'index',
+    // 通过回调修改tooltips的标题
+            callbacks: {
+                title: function (item) {
+                  return item[0].xLabel
+                }
+              }
+            }
+          }
+
+        }
+});
+});
+});
+});
